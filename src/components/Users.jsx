c21053898eb927022c614a70ad048efc8ece90df
NS-Sheam <123sheamfeni@gmail.com>
2023-05-07 21:04:31 +0600

    add create and delete operation

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 
 const Users = () => {
-    const users = useLoaderData();
+    const loadedUsers = useLoaderData();
+    const [users, setUsers] = useState(loadedUsers);
+    const handleDelete = _id => {
+        console.log("delete", _id);
+        fetch(`http://localhost:5000/users/${_id}`, {
+            method: "DELETE"
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if(data.deletedCount > 0){
+                    alert("deleted successfully");
+                    const remaining = users.filter(user => user._id !== _id);
+                    setUsers(remaining);
+                }
+            })
+    }
     return (
         <div>
             <h2>{users.length}</h2>
             <div>
                 {
                     users.map(user => (
-                        <p key={user._id}>{user.name} : {user.email}</p>
+                        <p key={user._id}
+                        >{user.name} : {user.email} : {user._id} <button
+                            onClick={() => handleDelete(user._id)}
+                        >X</button></p>
                     ))
                 }
             </div>
