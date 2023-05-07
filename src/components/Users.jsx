d0417fab09f9a8eadb6c1edbd774bef42824296b
NS-Sheam <123sheamfeni@gmail.com>
2023-05-07 21:42:45 +0600

    adding update operation of crud

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useLoaderData } from 'react-router-dom';
+import { Link, useLoaderData } from 'react-router-dom';
 
 const Users = () => {
     const loadedUsers = useLoaderData();
@@ -26,7 +26,11 @@ const Users = () => {
                 {
                     users.map(user => (
                         <p key={user._id}
-                        >{user.name} : {user.email} : {user._id} <button
+                        >{user.name} : {user.email} : {user._id} 
+                        <Link to={`/update/${user._id}`}>
+                        <button>Update</button>
+                        </Link>
+                        <button
                             onClick={() => handleDelete(user._id)}
                         >X</button></p>
                     ))
